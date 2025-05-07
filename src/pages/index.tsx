29ccf6b0c56a081cd2ba14b3e497357e77c84d87
tisou1 <57515396+tisou1@users.noreply.github.com>
2025-05-07 22:25:51 +0800

    chore: update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,6 @@ function Index() {
         value={value}
         onChange={e => setValue(e.target.value)}
       />
-      hello
 
       <div className="mt-3">
         <button className="btn" onClick={go}>go</button>
